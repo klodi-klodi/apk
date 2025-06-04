4c2f752fef6cffc73b443e3788ef89ecb7b2db4c
shqip_tv.al <kpatlli@yahoo.com>
2025-06-04 12:12:13 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Proxy URL to bypass CORS policy
     const proxyURL = 'https://api.allorigins.win/get?url=';
-    const playlistURL = 'https://github.com/klodi-klodi/apk/blob/main/Sample.m3u?raw=true';
+    const playlistURL = 'https://githubusercontent.com/Lorensshellira/albania-tv/refs/heads/main/Albtv?raw=true';
     const proxyPlaylistURL = proxyURL + encodeURIComponent(playlistURL);
 
     fetch(proxyPlaylistURL)
